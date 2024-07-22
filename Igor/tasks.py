8bf23a767acb74d1ee68a2f29590d070536eb0b8
Игорь <degtyannikov89@bk.ru>
2024-07-22 10:56:36 +0300

    Added : solved new tasks

---
+++ Igor/tasks.py
@@ -0,0 +1,39 @@
+# task_50
+# age = int(input('Please enter your age: '))
+# print(f"you'll be 100 years old in: {100 - age}")
+
+# task_51
+# colour = input("What's your favourite colour")
+# print(f'The colour: {color} - is a good colour')
+
+# task_52
+# proposal = input()
+# print(len(proposal))
+
+# task_53
+# while True:
+#     num1, num2, operator = float(input()), float(input()), input()
+#     if operator == '+':
+#         plus = num1 + num2
+#         print(f'Your answer: {plus}')
+#     elif operator == '-':
+#         minus = num1 - num2
+#         print(f'Your answer: {minus}')
+#     elif operator == '*':
+#         multiply = num1 * num2
+#         print(f'Your answer: {multiply}')
+#     elif operator == '/':
+#         if num2 == 0:
+#             print("Idiot! It's not divisible by 0")
+#         else:
+#             division = num1 / num2
+#             print(f'Your answer: {division}')
+#     else:
+#         print("Wrong operator:")
+#     again = input('Do you want to perform another calculation? (yes/no): ')
+#     if again == 'no':
+#         break
+
+#task_54 - сделать
+
+
